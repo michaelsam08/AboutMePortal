6d7699fbcf1e254d1da894c15c648c5b540c7c4a
michaelsam08 <32653861+michaelsam08@users.noreply.github.com>
2019-08-15 14:58:04 -0500

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//connects to mongoDB atlas  // set up an env file!!!!
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+// pulls in routes and create variables to use
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
